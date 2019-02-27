bd0f9ec972459becba0f0d00ba9c4030fd80119a
Robert Glover <robert_f_g@hotmail.com>
2019-02-27 18:39:33 -0500

    Adde PokeList and PokeParser

---
+++ csharp-class-project/PokeList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace csharp_class_project
+{
+    public class Rootobject
+    {
+        public int count { get; set; }
+        public object next { get; set; }
+        public object previous { get; set; }
+        public PokeList[] pokemonList { get; set; }
+    }
+
+    public class PokeList
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+}
--- csharp-class-project/PokeParser.cs
+++ csharp-class-project/PokeParser.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace csharp_class_project
 {
-    class Program
+    class PokeParser : IPokeParser
     {
-        static void Main(string[] args)
-        {
-            Console.ReadKey();
-        }
+
     }
 }
